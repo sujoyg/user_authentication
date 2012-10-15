8f13d6ed155cce045f2cd9f5d2f34c9c536dcdb2
Sujoy Gupta <sujoy@pingbooth.com>
2012-10-15 02:38:43 -0700

    Load the model from the app if present.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,6 @@ class User < ActiveRecord::Base
   before_validation { |user| user.password = Random.password if user.password.nil? }
   validates_presence_of :email
 end
+
+app_model = File.join Rails.root, 'app/models/user.rb'
+require app_model if File.exists? app_model
